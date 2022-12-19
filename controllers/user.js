c6fda05a9aa738280f44e350dbd17ca377a88c85
Edainsv <edainsv.pro@gmail.com>
2022-12-19 11:29:10 +0100

    création token d'authentification

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
@@ -28,8 +29,12 @@ exports.login = (req, res, next) => {
 	               return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
 				}
 	            res.status(200).json({
-	               userId: user._id,
-	               token: 'TOKEN'
+					userId: user._id,
+					token: jwt.sign(
+						{ userId: user._id },
+						'RANDOM_TOKEN_SECRET',
+						{ expiresIn: '24h' }
+					)
 	           	});
 	       })
 	       .catch(error => res.status(500).json({ error }));
